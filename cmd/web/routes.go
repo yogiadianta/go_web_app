7f4024582131d66b99bff8bfae1ae811c9768e6b
Yogi Adianta <yogiadianta0@gmail.com>
2022-07-27 20:48:06 +0700

    Using chi module for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,18 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/yogiadianta/go_web_app/pkg/config"
 	"github.com/yogiadianta/go_web_app/pkg/handlers"
 )
 
 func routes (app *config.AppConfig) http.Handler {
-    mux := pat.New()
+    mux := chi.NewRouter()
 
-    mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-    mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+    mux.Use(middleware.Recoverer)
 
+    mux.Get("/", handlers.Repo.Home)
+    mux.Get("/about", handlers.Repo.About)
     return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/yogiadianta/go_web_app
 
 go 1.18
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi v1.5.4
