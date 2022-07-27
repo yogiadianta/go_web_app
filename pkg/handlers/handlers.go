4836a5984c68db116a70954fb180bf994e016b53
Yogi Adianta <yogiadianta0@gmail.com>
2022-07-28 00:00:17 +0700

    Make example of usage of sessions

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -30,6 +30,9 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+    remoteIP := r.RemoteAddr
+    m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
     render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
@@ -38,6 +41,9 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
     // perform some logic
     stringMap := make(map[string]string)
     stringMap["test"] = "Hello, again."
+
+    remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+    stringMap["remote_ip"] = remoteIP
 
     // send the data  to the template
     render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
